068095bafaeaf338c67165e02f134d61ac33deb9
Rushiraj11 <grushiraj95@gmail.com>
2021-08-25 19:55:22 +0530

    Added: EmpWage using SwitchCase

--- src/main/java/com/bridgelabz/EmployeeWageBuilder.java
+++ src/main/java/com/bridgelabz/EmployeeWageBuilder.java
@@ -3,23 +3,27 @@ package com.bridgelabz;
 
 public class EmployeeWageBuilder
 {
+    // constant Variables
+    public static final int IS_FULL_TIME = 1;
+    public static final int IS_PART_TIME = 2;
+    public static final int EMP_RATE_PER_HOUR = 20;
+
     public static void main( String[] args ) {
-        int IS_FULL_TIME = 1;
-        int IS_PART_TIME = 2;
-        int EMP_RATE_PER_HOUR = 20;
+        // Variables
         int empHrs = 0;
         int empWage = 0;
 
-        double empCheck = Math.floor(Math.random() * 10) % 3;
-        if (empCheck == IS_FULL_TIME) {
+        int empCheck = (int) Math.floor(Math.random() * 10) % 3;
+        switch(empCheck) {
+        case IS_FULL_TIME:
             empHrs = 8;
-            System.out.println("Employee working Full Time");
-        } else if (empCheck == IS_PART_TIME) {
+            break;
+            case IS_PART_TIME:
             empHrs = 4;
-            System.out.println("Employee working Part Time");
-        } else {
+            break;
+            default:
             empHrs = 0;
-            System.out.println("Employee Absent");
+
         }
 
         empWage =empHrs * EMP_RATE_PER_HOUR;
