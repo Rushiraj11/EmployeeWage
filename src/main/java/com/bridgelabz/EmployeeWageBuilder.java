59f61f5b30ffe93d0e605cd595fc029047b630e6
Rushiraj11 <grushiraj95@gmail.com>
2021-08-25 17:54:51 +0530

    Added: EmpAttendence check

--- src/main/java/com/bridgelabz/EmployeeWageBuilder.java
+++ src/main/java/com/bridgelabz/EmployeeWageBuilder.java
@@ -6,5 +6,13 @@ public class EmployeeWageBuilder
     public static void main( String[] args )
     {
         System.out.println("Welcome to Employee wage");
+
+        int IS_FULL_TIME = 1;
+
+        double empCheck = Math.floor(Math.random() * 10) % 2;
+        if (empCheck == IS_FULL_TIME)
+            System.out.println("Employee is present");
+        else
+            System.out.println("Employee is Absent");
     }
 }
