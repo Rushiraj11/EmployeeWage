60f9ba6eed66f84de67b6a8b6452d57b1bd95a1e
Rushiraj11 <grushiraj95@gmail.com>
2021-08-25 18:53:09 +0530

    Added:EmpDailyWageCal

--- src/main/java/com/bridgelabz/EmployeeWageBuilder.java
+++ src/main/java/com/bridgelabz/EmployeeWageBuilder.java
@@ -5,14 +5,20 @@ public class EmployeeWageBuilder
 {
     public static void main( String[] args )
     {
-        System.out.println("Welcome to Employee wage");
-
         int IS_FULL_TIME = 1;
+        int EMP_RATE_PER_HOUR = 20;
+        int empHrs = 0;
+        int empWage = 0;
 
         double empCheck = Math.floor(Math.random() * 10) % 2;
-        if (empCheck == IS_FULL_TIME)
+        if (empCheck == IS_FULL_TIME) {
             System.out.println("Employee is present");
-        else
+            empHrs = 8;
+        }else {
             System.out.println("Employee is Absent");
+            empHrs = 0;
+        }
+        empWage =empHrs * EMP_RATE_PER_HOUR;
+        System.out.println("Emp Wage: " + empWage);
     }
 }
