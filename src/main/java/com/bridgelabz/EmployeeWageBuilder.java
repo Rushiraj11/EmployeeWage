53d8237a3381b656d89fbc98300a5caca9414607
Rushiraj11 <grushiraj95@gmail.com>
2021-08-25 19:32:23 +0530

    Added : EmpPartTimeWage

--- src/main/java/com/bridgelabz/EmployeeWageBuilder.java
+++ src/main/java/com/bridgelabz/EmployeeWageBuilder.java
@@ -3,21 +3,25 @@ package com.bridgelabz;
 
 public class EmployeeWageBuilder
 {
-    public static void main( String[] args )
-    {
+    public static void main( String[] args ) {
         int IS_FULL_TIME = 1;
+        int IS_PART_TIME = 2;
         int EMP_RATE_PER_HOUR = 20;
         int empHrs = 0;
         int empWage = 0;
 
-        double empCheck = Math.floor(Math.random() * 10) % 2;
+        double empCheck = Math.floor(Math.random() * 10) % 3;
         if (empCheck == IS_FULL_TIME) {
-            System.out.println("Employee is present");
             empHrs = 8;
-        }else {
-            System.out.println("Employee is Absent");
+            System.out.println("Employee working Full Time");
+        } else if (empCheck == IS_PART_TIME) {
+            empHrs = 4;
+            System.out.println("Employee working Part Time");
+        } else {
             empHrs = 0;
+            System.out.println("Employee Absent");
         }
+
         empWage =empHrs * EMP_RATE_PER_HOUR;
         System.out.println("Emp Wage: " + empWage);
     }
